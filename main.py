1192a116640068070bb3c2c8c2ea62e4578b7c4f
Arthur Dupont <adupont@yandex.com>
2022-10-21 14:17:57 +0200

    import render_template + templates directory added

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import url_for
+from flask import render_template
 
 app = Flask(__name__)
 
 app.secret_key = 'b05c5dafc4a00a8b7548d8fb35e45c1a86553f4f233b15f2d211d1deade97df9'
 
 
-@app.route("/qweqweqweqwe")
+@app.route("/")
 def index():
     return "Index Page"
 
