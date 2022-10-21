98cbbb727159c44d725373f62f498a853a6c36ba
Richard Cotte <rcotte1582@gmail.com>
2022-10-21 11:58:45 +0200

    Testing flask framework

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('test')
+from flask import Flask
+from flask import url_for
+
+app = Flask(__name__)
+
+app.secret_key = 'b05c5dafc4a00a8b7548d8fb35e45c1a86553f4f233b15f2d211d1deade97df9'
+
+
+@app.route("/qweqweqweqwe")
+def index():
+    return "Index Page"
+
+
+@app.route("/test/")
+def test():
+    return "<p>Test !</p>"
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+
+with app.test_request_context():
+    print(url_for('profile', username='John Doe'))
+    print(url_for('index'))
