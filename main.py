23aebf31e1c0cba7e7023e325e4c7643d94c22e1
Arthur Dupont <adupont@yandex.com>
2022-11-07 09:32:56 +0100

    ajout de correctifs

--- main.py
+++ main.py
@@ -14,25 +14,30 @@ app.secret_key = 'b05c5dafc4a00a8b7548d8fb35e45c1a86553f4f233b15f2d211d1deade97d
 @app.route('/index')
 def index():
     name = 'John Doe'
-    return render_template('index.html', title='Accueil - vous êtes connecté', logo='PowerFourFlash' username=name)
+    return render_template('index.html', title='Accueil - vous êtes connecté', logo='PowerFourFlash', username=name)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['email'] != 'admin' or request.form['password'] != 'admin':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Identifiants incorrects. Réessayez.'
         else:
             return redirect(url_for('index'))
-    return render_template('login.html', title='Créer mon compte' error=error)
+    return render_template('login.html', title='Créer mon compte', error=error)
 
-@app.route('/signup')
+@app.route('/signup' methods =['GET', 'POST'])
 def signup():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
     return render_template('signup.html', title='Créer mon compte')
 
 with app.test_request_context():
-    print(url_for('', username='John Doe'))
+    #print(url_for('', username='John Doe'))
     print(url_for('index'))
     print(url_for('login'))
     print(url_for('signup'))
