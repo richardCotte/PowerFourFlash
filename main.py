5c8eccbd8877d0a192a26d2e283efa254cba0ba4
Richard Cotte <rcotte1582@gmail.com>
2022-11-07 10:56:27 +0100

    Basic display of power four

--- main.py
+++ main.py
@@ -1,43 +1,44 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask import url_for
 from flask import flash
 from flask import session
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 app.secret_key = 'b05c5dafc4a00a8b7548d8fb35e45c1a86553f4f233b15f2d211d1deade97df9'
 
 
-def calculate_power_four_grid(table, chosen_column, playing_player_number):
-    playing_player_mark = "X" if playing_player_number == 1 else "O"
+def calculate_power_four_grid(table, chosen_column, playing_player):
     if 6 >= chosen_column >= 0:
         for line in table:
             if line[chosen_column] == ".":
-                line[chosen_column] = playing_player_mark
-                break
-        else:
-            flash("The chosen column is full")
+                line[chosen_column] = playing_player
+                return True
+        flash("The chosen column is full")
+        return False
     else:
-        flash("Pick a number between 0 and 6 please")
+        flash("The chosen column must be between 0 and 6")
+        return False
 
 
-@app.route("/power_four/<chosen_grid>")
-def power_four(chosen_grid):
-    if 'grid' and 'playing_player' in session:
-        calculate_power_four_grid(session['grid'], int(chosen_grid), session['playing_player'])
+@app.route("/power_four/", methods=['POST', 'GET'])
+def power_four():
+    if 'grid' not in session:
+        session['grid'] = [["."] * 7 for i in range(6)]
+        session['playing_player'] = "X"
+    if request.method == 'POST':
+        chosen_grid = request.form['chosen_grid']
+        if calculate_power_four_grid(session['grid'], int(chosen_grid), session['playing_player']):
+            session['playing_player'] = "X" if session['playing_player'] == "O" else "O"
 
-        # We are storing our grid in session, and we modify a list in a dictionary so the session doesn't see that
+        # We are storing our grid in session, and we modify a list in a dictionary however the session doesn't see that
         # the session variable has been changed, so we need to 'force' update it
         session['grid'] = session['grid']
+        return redirect(url_for('power_four'))
 
-        session['playing_player'] = 1 if session['playing_player'] == 2 else 2
-
-        return render_template('power_four.html', grid=session['grid'])
-    else:
-        session['playing_player'] = 1
-        session['grid'] = [["."] * 7 for i in range(6)]
-        return render_template('power_four.html', grid=session['grid'])
+    return render_template('power_four.html', grid=session['grid'], playing_player=session['playing_player'])
 
 
 @app.route("/")
