86f1f940b8f19c4becd1a28894bfdf702f3f4853
Joris Thivard <Joris.Thivard@gmail.com>
2022-11-07 15:19:16 +0100

    System enregistrement joueur

--- main.py
+++ main.py
@@ -1,38 +1,55 @@
-<<<<<<< HEAD
 from importlib.resources import path
-from flask import Flask
-=======
 from flask import Flask, redirect
->>>>>>> origin/powerFourBack
 from flask import url_for
 from flask import flash
 from flask import session
 from flask import render_template
-<<<<<<< HEAD
+from flask import request
 from flask import current_app, g
 from pathlib import Path
 from flask import redirect
 from flask import request
 import sqlite3
-=======
-from flask import request
->>>>>>> origin/powerFourBack
 
 app = Flask(__name__)
 
-app.secret_key = 'b05c5dafc4a00a8b7548d8fb35e45c1a86553f4f233b15f2d211d1deade97df9'
+app.secret_key = "b05c5dafc4a00a8b7548d8fb35e45c1a86553f4f233b15f2d211d1deade97df9"
 
 
-<<<<<<< HEAD
+@app.route("/sinup_player", methods=["POST", "GET"])
+def sinup_player():
+    username = request.form["username"]
+    email = request.form["email"]
+    password = request.form["password"]
+    if len(username) > 0 and len(email) > 0 and len(password) > 0:
+        db = get_db()
+        cur = db.cursor()
+        sqlRequest = (
+            "INSERT INTO player (email, pseudo, pass) VALUES ('"
+            + str(email)
+            + "', '"
+            + str(username)
+            + "', '"
+            + str(password)
+            + "')"
+        )
+        cur.execute(sqlRequest)
+        db.commit()
+        return redirect(url_for("login"))
+    else:
+        return "<p>ERROR</p>"
+
+
 def get_db():
     db = sqlite3.connect("database.db")
     db.row_factory = sqlite3.Row
     return db
 
 
-if not Path("database.db").exists() :
+if not Path("database.db").exists():
     get_db().executescript(Path("SCRIPT.sql").read_text())
-=======
+
+
 def calculate_power_four_grid(table, chosen_column, playing_player):
     if 6 >= chosen_column >= 0:
         for line in table:
@@ -46,58 +63,82 @@ def calculate_power_four_grid(table, chosen_column, playing_player):
         return False
 
 
-@app.route("/power_four/", methods=['POST', 'GET'])
+@app.route("/power_four/", methods=["POST", "GET"])
 def power_four():
-    if 'grid' not in session:
-        session['grid'] = [["."] * 7 for i in range(6)]
-        session['playing_player'] = "X"
-    if request.method == 'POST':
-        chosen_grid = request.form['chosen_grid']
-        if calculate_power_four_grid(session['grid'], int(chosen_grid), session['playing_player']):
-            session['playing_player'] = "X" if session['playing_player'] == "O" else "O"
+    if "grid" not in session:
+        session["grid"] = [["."] * 7 for i in range(6)]
+        session["playing_player"] = "X"
+    if request.method == "POST":
+        chosen_grid = request.form["chosen_grid"]
+        if calculate_power_four_grid(
+            session["grid"], int(chosen_grid), session["playing_player"]
+        ):
+            session["playing_player"] = "X" if session["playing_player"] == "O" else "O"
 
         # We are storing our grid in session, and we modify a list in a dictionary however the session doesn't see that
         # the session variable has been changed, so we need to 'force' update it
-        session['grid'] = session['grid']
-        return redirect(url_for('power_four'))
+        session["grid"] = session["grid"]
+        return redirect(url_for("power_four"))
 
-    return render_template('power_four.html', grid=session['grid'], playing_player=session['playing_player'])
->>>>>>> origin/powerFourBack
+    return render_template(
+        "power_four.html",
+        grid=session["grid"],
+        playing_player=session["playing_player"],
+    )
 
 
 @app.route("/")
-@app.route('/index')
+@app.route("/index")
 def index():
-    name =""
-    return render_template('index.html', title='Accueil - vous êtes connecté', logo='PowerFourFlash', username=name)
+    name = ""
+    return render_template(
+        "index.html",
+        title="Accueil - vous êtes connecté",
+        logo="PowerFourFlash",
+        username=name,
+    )
 
-@app.route('/login', methods=['POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Identifiants incorrects. Réessayez.'
+    if request.method == "POST":
+        if request.form["username"] != "admin" or request.form["password"] != "admin":
+            error = "Identifiants incorrects. Réessayez."
         else:
-            return redirect(url_for('index'))
-    return render_template('login.html', title='Créer mon compte', error=error)
+            return redirect(url_for("index"))
+    return render_template("login.html", title="Créer mon compte", error=error)
 
-@app.route('/signup', methods =['GET', 'POST'])
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-    return render_template('signup.html', title='Créer mon compte')
+    # msg = ""
+    # username = request.form["username"]
+    # email = request.form["email"]
+    # password = request.form["password"]
+    # if(len(username) > 0 and len(email) > 0 and len(password) > 0):
+    # db = get_db()
+    # sqlRequest = "INSERT INTO %s VALUES ('%s', '%s', '%s')" % (email, username, password)
+    # db.execute(sqlRequest)
+    # if (
+    # request.method == "POST"
+    # and "username" in request.form[""]
+    # and "password" in request.form[""]
+    # and "email" in request.form[""]
+    # ):
+    # username = request.form["username"]
+    # password = request.form["password"]
+    # email = request.form["email"]
+    return render_template("signup.html", title="Créer mon compte")
 
 
-@app.route('/user/<username>')
+@app.route("/user/<username>")
 def profile(username):
-    return f'{username}\'s profile'
+    return f"{username}'s profile"
 
 
 with app.test_request_context():
-    print(url_for('profile', username='John Doe'))
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('signup'))
+    print(url_for("profile", username="John Doe"))
+    print(url_for("index"))
+    print(url_for("login"))
+    print(url_for("signup"))
